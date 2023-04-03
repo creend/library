3e2ec48d9ad19bdbd6fcefe4ef1880726b3185ab
Creend <creend42@gmail.com>
2023-04-03 20:09:03 +0200

    Add book router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { authRouter } from "./routers/auth";
+import { booksRouter } from "./routers/books";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { authRouter } from "./routers/auth";
  */
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  books: booksRouter,
 });
 
 // export type definition of API
