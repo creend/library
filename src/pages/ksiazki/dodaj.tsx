3eea7681d990188f06012ec7d61e088bc0d46215
Creend <creend42@gmail.com>
2023-04-04 19:09:26 +0200

    create initial form

--- src/pages/ksiazki/dodaj.tsx
+++ src/pages/ksiazki/dodaj.tsx
@@ -1,4 +1,7 @@
+import { Form, Formik } from "formik";
 import * as Yup from "yup";
+import Input from "~/components/input";
+import Spinner from "~/components/spinner";
 
 const AddBookSchema = Yup.object().shape({
   author: Yup.string()
@@ -29,10 +32,61 @@ const initialValues = {
 };
 
 const AddBookPage = () => {
+  const isLoading = false;
   return (
-    <div>
-      <p>e</p>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={AddBookSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form
+        className={`relative mx-auto mt-11 w-3/4 max-w-xl rounded-2xl  p-10 
+          ${isLoading ? "bg-gray-800 hover:bg-gray-700" : "bg-gray-900"}`}
+        autoComplete="off"
+      >
+        {isLoading && <Spinner />}
+
+        <h3 className="mb-10 text-2xl font-semibold text-slate-200">
+          Dodawanie książki
+        </h3>
+        <Input input={{ name: "author", id: "author" }} label="Autor" />
+        <Input input={{ name: "title", id: "title" }} label="Tytuł" />
+        <Input
+          input={{
+            name: "publisher",
+            id: "publisher",
+          }}
+          label="Wydawnictwo"
+        />
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <Input
+            input={{
+              name: "yearOfRelease",
+              id: "yearOfRelease",
+              type: "number",
+            }}
+            label="Rok wydania"
+          />
+          <Input
+            input={{
+              name: "availableCopies",
+              id: "availableCopies",
+              type: "number",
+            }}
+            label="Dostępne egzemplarze"
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4  focus:ring-blue-800 sm:w-auto"
+        >
+          Dodaj
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
