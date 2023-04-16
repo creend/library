b76b7d748457dd49429a41ee8149b82ad0b94cea
Creend <creend42@gmail.com>
2023-04-16 10:10:18 +0200

    Hamburger menu for <1024px width

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,7 @@
 import { signOut, useSession } from "next-auth/react";
 import AdminDashboard from "./admin-dashboard";
 import UserDashboard from "./user-dashboard";
-import { type ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import DashboardItem from "./dashboard-item";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { toast } from "react-hot-toast";
@@ -13,6 +13,8 @@ const Layout = ({ children }: { children: ReactNode }) => {
     session.data?.user.role === "admin" ? AdminDashboard : UserDashboard;
   const isLoggedIn = session.status === "authenticated";
 
+  const [isHamburgerMenuOpened, setIsHamnurgerMenuOpened] = useState(false);
+
   if (session.status === "loading")
     return (
       <div className="h-screen">
@@ -21,13 +23,41 @@ const Layout = ({ children }: { children: ReactNode }) => {
     );
   return (
     <>
+      <button
+        data-collapse-toggle="navbar-hamburger"
+        type="button"
+        className={`fixed left-0 top-0 z-50 ml-3 inline-flex  items-center rounded-lg p-2 text-sm text-gray-400 transition-transform hover:bg-gray-700 focus:outline-none focus:ring-2  focus:ring-gray-600 lg:hidden `}
+        aria-controls="navbar-hamburger"
+        aria-expanded="false"
+        onClick={() => setIsHamnurgerMenuOpened((prevState) => !prevState)}
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="h-6 w-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
       <aside
         id="sidebar-multi-level-sidebar"
-        // className="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0"
-        className="fixed left-0 top-0 z-40 h-screen w-64"
+        className={`fixed left-0 top-0 z-40 h-screen w-full -translate-x-full transition-transform lg:w-64 ${
+          isHamburgerMenuOpened ? "translate-x-0" : "lg:translate-x-0"
+        } `}
         aria-label="Sidebar"
       >
-        <div className="h-full overflow-y-auto bg-gray-800 px-3 py-4">
+        <div
+          className={`h-full overflow-y-auto bg-gray-800 px-3 py-4 ${
+            isHamburgerMenuOpened ? "pt-14" : ""
+          } `}
+        >
           <ul className="space-y-2 font-medium">
             <Dashboard />
             {isLoggedIn ? (
@@ -48,7 +78,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
           </ul>
         </div>
       </aside>
-      <main className="pl-64">{children}</main>
+      <main className="lg:pl-64">{children}</main>
     </>
   );
 };
